62fb886b29b26087736f95c97a01b6b121b9d4b5
daprofbshop <64336783+daprofbshop@users.noreply.github.com>
2020-06-10 11:41:37 +0300

    1
    1

---
+++ guida-family.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400,300,600,700' rel='stylesheet'>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mini guida alla vacanza family</title>
+<meta name="description" content="Responsive Ebook Landing Page">
+<link rel="shortcut icon" href="/guida-family/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/guida-family/img/favicon.ico" type="image/x-icon">
+<link href="/guida-family/css/styles.min.css" rel="stylesheet">
+<link href="/guida-family/css/main.css" rel="stylesheet">
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-24010768-1', 'auto');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview');
+</script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+
+<!--Hero section-->
+<section class="hero" data-0="background-position:0px 0px;" data-500="background-position:0px -250px;">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="logo"> 
+          <!--logo--> 
+          <a href="https://www.italyfamilyhotels.it" title="Italy Family Hotels" target="_blank"><img src="/guida-family/img/logo.png" data-at2x="/guida-family/img/logo@2x.png" alt="Italy Family Hotels"> </a> 
+          <!--logo end--> 
+        </div>
+      </div>
+    </div>
+    <div class="row" data-0="opacity: 1" data-800="opacity: 0">
+      <div class="col-md-7"> 
+        <!--welcome message-->
+        <header class="welcome-message">
+          	<h1>Organizzare vacanze con i bambini è un’arte</h1>
+          	<h2>Ma facile e veloce da imparare!</h2>
+          	<p>Tre mamme blogger che in vacanza ne hanno passate tante con i loro bimbi hanno deciso di semplificare la vita alle mamme come te e hanno realizzato questa 
+          	<strong>super mini guida ideale per</strong>:</p>
+          	<p><img src="/guida-family/img/pallino.png" width="12" height="12" alt="*" style="margin-right:10px" /> scegliere la località italiana ideale per tutta la famiglia<br />
+         	<img src="/guida-family/img/pallino.png" width="12" height="12" alt="*" style="margin-right:10px" /> evitare imprevisti e godersi tranquilli la vacanza con i bambini<br />
+         	<img src="/guida-family/img/pallino.png" width="12" height="12" alt="*" style="margin-right:10px" /> avere idee e consigli su dove andare e cosa vedere...</p>
+            <p>...perché la vacanza deve essere divertimento per tutti!</p>
+        </header>
+        <!--welcome message end--> 
+        
+        <!--action button-->
+        <div class="action-button"> 
+        	<a href="#iscrizione" class="button learn-more text-center">Scarica la guida</a>
+        </div>
+        <!--action button end--> 
+        
+      </div>
+      <div class="col-md-5 text-center"> <img src="/guida-family/img/miniguida.png" alt="miniguida" class="book-cover" data-no-retina> </div>
+    </div>
+  </div>
+</section>
+<!--Hero section end--> 
+
+<!--reviews-->
+<section class="reviews section-spacing text-center">
+  <div class="container">
+    <h2>Le nostre Blogger</h2>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="review-slider"> 
+          <!--review 1-->
+          <div>
+            <blockquote class="col-md-10 center-block">
+              <p> <i class="fa fa-quote-left"></i> Equilibrista di mestiere. Perchè, come quasi tutte le donne, vivo alla perenne ricerca di un incastro (im)perfetto tra il lavoro, la famiglia, la casa e i miei sogni nel cassetto. Due li realizzo di tanto in tanto: viaggiare e scrivere. Come riesco a fare tutto? Non faccio niente bene, ma lo faccio con il cuore in mano. A qualcuno comunque piace. <i class="fa fa-quote-right"></i> </p>
+            </blockquote>
+            <figure><img src="/guida-family/img/valentina-gattei.png" alt="face" class="img-circle">
+              <h4>Valentina Gattei</h4>
+            </figure>
+          </div>
+          <!--review 1 end--> 
+          
+          <!--review 2-->
+          <div>
+            <blockquote class="col-md-10 center-block">
+              <p> <i class="fa fa-quote-left"></i> Brianzola, con la passione per le Langhe e un pezzettino di cuore lasciato in quel di Bergamo, amo viaggiare e condividere questa passione con mio figlio. Mamma di un Patato che sta crescendo in fretta, sono convinta che i viaggi aiutino a crescere e sono certa di aver imparato a farlo leggendo. Per questo ora che sono mamma continuo a viaggiare e leggere, scrivendo i miei racconti di viaggio sul mio blog www.patatofriendly.com e anche in forma di fiabe per l’infanzia.  <i class="fa fa-quote-right"></i> </p>
+            </blockquote>
+            <figure><img src="/guida-family/img/francesca-taioli.png" alt="face" class="img-circle">
+              <h4>Francesca taioli</h4>
+            </figure>
+          </div>
+          <!--review 2 end--> 
+          
+          <!--review 3-->
+          <div>
+            <blockquote class="col-md-10 center-block">
+              <p> <i class="fa fa-quote-left"></i> Fiorentina DOC, curiosa di natura e multitasking per necessità, sono moglie e mamma di una piccola birbante di tre anni a cui spero di trasmettere la passione per i viaggi e per la nostra bella Toscana. Potete seguirmi anche su MammeaFirenze.it. <i class="fa fa-quote-right"></i> </p>
+            </blockquote>
+            <figure><img src="/guida-family/img/cristina-romeo.png" alt="face" class="img-circle">
+              <h4>Cristina Romeo</h4>
+            </figure>
+          </div>
+          <!--review 3 end--> 
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--reviews end--> 
+
+<!--sub form-->
+<section class="sub-form section-spacing text-center">
+  <a name="iscrizione"></a>
+  <div class="container">
+    <header>
+      <h2>Scarica la mini guida</h2>
+      <h3>Iscriviti alla newsletter di Italy Family Hotels</h3>
+		<p style="color:#000; font-size:18px;">Ricevi subito la mini guida “La mia famiglia in vacanza” (in formato PDF pronto per essere stampato)<br />
+		Ricevi periodicamente notizie coinvolgenti e offerte interessanti per le tue vacanze in tutta Italia</p>
+    </header>
+    <div class="row">
+      <div class="col-md-8"> <img src="/guida-family/img/miniguida-footer.png" alt="ebook preview" data-no-retina> </div>
+      <div class="col-md-4">
+        <form method="post" role="form" id="mc-form" action="/guida-family/php/subscribe.php">
+          <div>
+            <input type="text" class="slide-bottom form-control" placeholder="Nome" name="fname" id="mailchimp-fname">
+            <i class="fa fa-user first-name-i"></i></div>
+          <div>
+            <input type="text" class="slide-bottom form-control" placeholder="Cognome" name="lname" id="mailchimp-lname">
+            <i class="fa fa-user last-name-i"></i></div>
+          <div>
+            <input type="email" class="slide-bottom form-control" placeholder="Email" name="email" id="mailchimp-email">
+            <i class="fa fa-envelope"></i></div>
+		  		  <div class="slide-bottom" id="div_marketing">
+			<img id="marketing_img" src="/guida-family/img/privacy_off.png" width="18" height="18" onclick="checkMarketing()" />
+    		<input id="marketing" name="marketing" type="checkbox" value="1" style="display:none" onchange="javascript:checkImageMarketing()" />
+			<label for="marketing">Acconsento il <a href="https://www.italyfamilyhotels.it/privacy-form" target="_blank">Consorzio Italy Family Hotels</a> e tutte le <a href="https://www.italyfamilyhotels.it/privacy-form" target="_blank">strutture associate (attuali e future)</a> a inviarmi newsletter e comunicazioni commerciali.<br />Potrò disiscrivermi in qualsiasi momento attraverso le comunicazioni ricevute.</label>
+		  </div>	
+		  <div class="slide-bottom" id="div_privacy">
+			<a href="https://www.italyfamilyhotels.it/privacy-form" target="_blank">Leggi qui l’informativa privacy in riferimento all’art. 13 del Regolamento UE 2016/679</a>
+		  </div>	
+          <button type="submit" class="slide-bottom btn btn-default">Scarica la mini guida</button>
+          <div id="form-recaptcha" class="g-recaptcha"
+        	data-sitekey="6LfdISYUAAAAAKswczUDn84mCSqyVW6XfWd19Dur"
+          	data-callback="onSubmit"
+          	data-size="invisible"></div>
+        </form>
+        <div class="result"></div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--sub form--> 
+
+<script type="text/javascript">
+	
+function checkMarketing() {
+	$('#marketing').prop('checked', $('#marketing').prop('checked') ? false : true );
+	checkImageMarketing();
+}
+function checkImageMarketing() {
+	if($('#marketing').prop('checked')) { 
+		$('#marketing_img').attr('src', '/guida-family/img/privacy_on.png');
+	} else { 
+		$('#marketing_img').attr('src', '/guida-family/img/privacy_off.png');
+	}
+}	
+</script>
+
+<!--site-footer-->
+<footer class="site-footer section-spacing">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p class="scroll-top"><a href="#"><i class="fa fa-chevron-up"></i></a></p>
+        
+        <!--social-->
+        
+        <ul class="social">
+          <li><a href="https://www.facebook.com/ItalyFamilyHotels" target="_blank"><i class="fa fa-facebook"></i></a></li>
+        </ul>
+        
+        <!--social end--> 
+        
+        <small>Italy Family Hotels - P.I. 03223190400 - email: <a href="mailto:info@italyfamilyhotels.it">info@italyfamilyhotels.it</a> - <a href="https://www.italyfamilyhotels.it" target="_blank">www.italyfamilyhotels.it</a>
+		  <br /><a href="https://www.italyfamilyhotels.it/privacy" title="Privacy Policy">Privacy Policy</a> - <a href="https://www.italyfamilyhotels.it/cookie-policy" title="Cookie Policy">Cookie Policy</a></small>
+                
+      </div>
+    </div>
+  </div>
+</footer>
+<!--site-footer end--> 
+
+<!--PRELOAD-->
+<div id="preloader">
+  <div id="status"></div>
+</div>
+<!--end PRELOAD--> 
+
+<script src="/guida-family/js/scripts.min.js"></script> 
+<script src="/guida-family/js/main.js"></script> 
+
+<div id="banda_cookie"><div>
+	<a href="javascript:void(0)" class="accetto" onclick="javascript:accettaCookie()">HO CAPITO</a>
+	<a href="https://www.italyfamilyhotels.it/cookie-policy" class="informativa">INFORMATIVA COOKIE</a>
+	<a href="https://www.italyfamilyhotels.it/privacy" class="informativa2">INFORMATIVA PRIVACY</a>
+    <p>La informiamo che, per migliorare la Sua esperienza di navigazione su questo sito web, il Consorzio ITALY FAMILY HOTELS (via Macanno 38/Q, 47923 Rimini (RN), info@italyfamilyhotels.it), in qualità di titolare del trattamento, utilizza diversi tipi di cookie, tra cui: Cookies tecnici o analitici prima parte; Cookies analitici di terze parti anonimi e non anonimi, Nella pagina dell’Informativa estesa "Cookie policy" che trovate all'indirizzo web www.italyfamiliyhotels.it sono presenti le istruzioni per negare il consenso all'installazione di qualunque tipologia di cookie. Cliccando su "HO CAPITO" o continuando la navigazione, dichiara di avere compreso le modalità nell'Informativa Estesa ai sensi dell'Art 13 del Reg. (UE) 2016/679.</p>
+</div></div>
+<script type="text/javascript">
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$(function(){
+	if(getCookie('viewed_cookie_policy')!=1) {
+		$('#banda_cookie').slideDown('fast');
+	}
+});
+	
+function accettaCookie() {
+	var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000)); // 1 anno
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = "viewed_cookie_policy=1;" + expires + ";path=/";
+	$.ajax({ url: '/consenso_cookie.php', data: { pagina: '/guida-family.php' } });
+	$('#banda_cookie').slideUp('fast');
+}
+</script>
+	
+</body>
+</html>
